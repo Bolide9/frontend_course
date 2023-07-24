2e53edc5672b82ed6bd4036db5fef52deda7ed66
Bolide9 <viskhaev.m@yandex.ru>
2023-07-24 17:33:18 +0300

    js lesson #1

--- yellow_kitchen_front/js/example.js
+++ yellow_kitchen_front/js/example.js
@@ -1,10 +1,10 @@
 // Вывод в консоль
-console.log("Привет, мир!");
+console.log('Hello, world!');
 
 // Создание переменных с правильными именами
 let correctName1 = 42;
-const correctName2 = "Hello, world!";
+const correctName2 = 'Hello, world!';
 
 // Создание переменных с неправильными именами
-let 123invalidName = true; // Имя переменной не может начинаться с цифры
-const #invalidName = "Oops!"; // Имя переменной не может начинаться с символа #
+// let 123invalidName = true; // Имя переменной не может начинаться с цифры
+// const #invalidName = "Oops!"; // Имя переменной не может начинаться с символа #
