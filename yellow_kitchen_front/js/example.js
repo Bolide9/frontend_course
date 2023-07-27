72b72ca41bf5b24f00ebcd572623fd8891f55972
Bolide9 <viskhaev.m@yandex.ru>
2023-07-27 14:34:34 +0300

    js lesson 3

--- yellow_kitchen_front/js/example.js
+++ yellow_kitchen_front/js/example.js
@@ -1,10 +1,26 @@
-// Вывод в консоль
-console.log('Hello, world!');
+// Тип данных "строка" (string)
+const date = Date(1999, 2, 28);
 
-// Создание переменных с правильными именами
-let correctName1 = 42;
-const correctName2 = 'Hello, world!';
+console.log(date);
 
-// Создание переменных с неправильными именами
-// let 123invalidName = true; // Имя переменной не может начинаться с цифры
-// const #invalidName = "Oops!"; // Имя переменной не может начинаться с символа #
+// Тип данных "число с плавающей запятой" (float)
+const pi = 3.14;
+
+console.log(`pi ${pi}`);
+
+const myConstant = "Hello";
+
+// Попытка изменить значение константы
+myConstant = "World"; // Вызовет ошибку, так как константу нельзя изменить.
+
+let name = prompt("Введите ваше имя:");
+let address = prompt("Введите ваш адрес:");
+let phone = prompt("Введите ваш номер телефона:");
+
+console.log(`Студент ${name}. Адрес: ${address}, Номер телефона: ${phone}`);
+
+let exchangeRate = prompt("Введите курс рубля к доллару:");
+let rublesAmount = prompt("Введите количество доступных рублей:");
+
+let dollarsAmount = rublesAmount / exchangeRate;
+console.log(`На доступные рубли можно купить ${dollarsAmount.toFixed(2)} долларов.`);
