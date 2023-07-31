5e57605860b0458ac1f82b9c3cf07eb92409b264
Bolide9 <viskhaev.m@yandex.ru>
2023-07-31 17:27:30 +0300

    js lesson #5

--- yellow_kitchen_front/js/example.js
+++ yellow_kitchen_front/js/example.js
@@ -1,49 +1,43 @@
 /// 1
 
-const myConstant = 5;
+function checkNumberConstant(number) {
+  if (number < 10) {
+    console.log("константа меньше 10");
+  } else {
+    console.log("константа не меньше 10");
+  }
+}
 
-myConstant < 10 ? console.log("константа меньше 10") : console.log("константа не меньше 10");
-
-///
+checkNumberConstant(5);
+checkNumberConstant(15);
 
 /// 2
 
-let x = 10,
-  y = 7;
+function showAlert() {
+  alert("Привет, Мир");
+}
 
-const result = x > y ? "x больше, чем y" : "x не больше, чем y";
-console.log(result);
-
-///
+showAlert();
 
 /// 3
 
-let num = prompt("Введите число");
-
-if (num % 2 === 0) {
-  console.log(`Число ${num} четное`);
-} else {
-  console.log(`Число ${num} нечетное`);
+function printJavaScript() {
+  console.log("JavaScript");
 }
 
-///
+printJavaScript();
 
 /// 4
 
-let num1 = +prompt("Введите первое число");
-let num2 = +prompt("Введите второе число");
-let num3 = +prompt("Введите третье число");
+let text = "Этот текст содержит lorem";
 
-if (num1 >= num2 && num1 >= num3) {
-  console.log(`Наибольшее число: ${num1}`);
-} else if (num2 >= num1 && num2 >= num3) {
-  console.log(`Наибольшее число: ${num2}`);
-} else {
-  console.log(`Наибольшее число: ${num3}`);
+function checkTextContent(str) {
+  if (str.includes("lorem")) {
+    console.log("У вас ошибка в тексте");
+  } else {
+    console.log("Содержимое переменной: " + str);
+  }
 }
 
-///
-
-/// 5
-
-///
+checkTextContent(text);
+checkTextContent("Этот текст не содержит");
